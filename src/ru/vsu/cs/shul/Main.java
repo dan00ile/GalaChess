901d80b30a14a82e2b82b7392740f7952f1e62ac
Даниил <125554382+dan00ile@users.noreply.github.com>
2023-12-20 23:17:54 +0300

    init Chess

--- src/ru/vsu/cs/shul/Main.java
+++ src/ru/vsu/cs/shul/Main.java
@@ -2,7 +2,7 @@ package ru.vsu.cs.shul;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world13213");
     }
 }
 
